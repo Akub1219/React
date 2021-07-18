db6bbc9aebbc83ab17eccfb5b70a9c6f4be3ae1f
Akub1219 <69302785+Akub1219@users.noreply.github.com>
2021-07-19 06:26:13 +0900

    ToDo-JS

---
+++ src/lesson.js
@@ -0,0 +1,209 @@
+/**
+ * const let などの変数宣言
+ */
+// var val1 = "var変数";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+// // let変数は上書き可能
+// val2 = "let変数上書き";
+// // let変数は再宣言不可能
+// let val2 = "let変数再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+// // const変数は上書きも不可能
+// val3 = "const変数上書き";
+// // let変数は再宣言不可能
+// let val3 = "const変数再宣言";
+
+//constで定義したオブジェクトはプロパディの変更が可能
+// const val4 = {
+//   name: "じゃけえ",
+//   age: "28"
+// };
+// val4.name = "jake";
+// val4.address = "hirosima";
+// console.log(val4);
+
+//constで定義した配列はプロパディの変更が可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "じゃけぇ";
+// const age = 28;
+
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+// console.log(message1);
+
+// //テンプレート文字列を用いる方法
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の方法
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// //アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => num1 + num2;
+// console.log(func3(3, 6));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28
+// };
+// // const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です`;
+// // console.log(message1);
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+// const myProfile = ["じゃけぇ", 28];
+// const message3 = `名前は${myProfile[0]}です、年齢は${myProfile[1]}です`;
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}さん`);
+// sayHello("じゃけぇ");
+
+/**
+ * スプレッド構文
+ */
+// //配列の展開
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// arr6[0] = 100;
+// console.log(arr6);
+// console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+//=で渡すと参照元の値もかきかえてしまう（なのでスプレッド構文で渡す）
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr4);
+
+/**
+ * mapやFilterを使った配列の処理
+ */
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件？条件がtrueのとき:上限がfalseのとき
+// const val1 = 1 > 0 ? `trueです` : `falseです`;
+// console.log(val1);
+
+// const num = 1300;
+
+// const formattedNum =
+//   typeof num === `number` ? num.toLocaleString() : `数値を入力して`;
+
+// console.log(formattedNum);
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? `100を超えてる` : `許容範囲`;
+// };
+// console.log(checkSum(50, 40));
+
+/***
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if (flag1 || flag2) {
+//   console.log("１か２はtrue");
+// }
+// if (flag1 && flag2) {
+//   console.log("１も２もtrue");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// //&&は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定された";
+// console.log(fee2);
